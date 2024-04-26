a721d5879609c0a507a1b6b0f3b28f285d69f90c
Hannah Bowles <hannah_m_bowles@outlook.com>
2024-04-26 09:37:18 +0000

    Feat: : Add UserProfileAdmin view for managing user profiles in the admin interface

--- profiles/admin.py
+++ profiles/admin.py
@@ -1,3 +1,29 @@
 from django.contrib import admin
+from .models import UserProfile
 
-# Register your models here.
+
+class UserProfileAdmin(admin.ModelAdmin):
+    """
+    Admin view for the UserProfile model.
+    """
+    fields = (
+        'user', 'default_phone_number', 'default_street_address1',
+        'default_street_address2', 'default_town_or_city', 'default_county',
+        'default_postcode', 'default_country',
+    )
+
+    list_display = (
+        'user', 'default_phone_number', 'default_street_address1',
+        'default_street_address2', 'default_town_or_city', 'default_county',
+        'default_postcode', 'default_country',
+    )
+
+    # Define fields to be searchable in the admin interface
+    search_fields = (
+        'user', 'default_phone_number', 'default_street_address1',
+        'default_street_address2', 'default_town_or_city', 'default_county',
+        'default_postcode', 'default_country',
+    )
+
+
+admin.site.register(UserProfile, UserProfileAdmin)
